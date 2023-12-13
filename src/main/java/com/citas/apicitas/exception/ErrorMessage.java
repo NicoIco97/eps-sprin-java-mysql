17e693fd8e7e913b1d3222ef4da78b87d6b04104
Nicolas Ico <132182589+NicoIco97@users.noreply.github.com>
2023-12-13 16:06:59 -0500

    Documentacion

--- src/main/java/com/citas/apicitas/exception/ErrorMessage.java
+++ src/main/java/com/citas/apicitas/exception/ErrorMessage.java
@@ -4,17 +4,44 @@ import java.util.Date;
 
 import lombok.Data;
 
+/**
+ * Clase que representa un mensaje de error personalizado para manejar excepciones.
+ */
 @Data
 public class ErrorMessage {
-  private int statusCode;
-  private Date timeStamp;
-  private String message;
-  private String description;
 
-  public ErrorMessage(int statusCode, Date timeStamp, String message, String description){
-    this.statusCode = statusCode;
-    this.timeStamp = timeStamp;
-    this.message = message;
-    this.description = description;
-  }
+    /**
+     * Código de estado HTTP asociado al error.
+     */
+    private int statusCode;
+
+    /**
+     * Marca de tiempo que indica cuándo ocurrió el error.
+     */
+    private Date timeStamp;
+
+    /**
+     * Mensaje descriptivo del error.
+     */
+    private String message;
+
+    /**
+     * Descripción detallada del error.
+     */
+    private String description;
+
+    /**
+     * Constructor que inicializa los atributos de ErrorMessage.
+     *
+     * @param statusCode Código de estado HTTP asociado al error.
+     * @param timeStamp  Marca de tiempo que indica cuándo ocurrió el error.
+     * @param message    Mensaje descriptivo del error.
+     * @param description Descripción detallada del error.
+     */
+    public ErrorMessage(int statusCode, Date timeStamp, String message, String description) {
+        this.statusCode = statusCode;
+        this.timeStamp = timeStamp;
+        this.message = message;
+        this.description = description;
+    }
 }
