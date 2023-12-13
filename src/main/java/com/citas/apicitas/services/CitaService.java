8cb0146778ff6f2d2ada53126b33e67bb2df2a31
Nicolas Ico <132182589+NicoIco97@users.noreply.github.com>
2023-12-13 16:03:24 -0500

    Documentacion

--- src/main/java/com/citas/apicitas/services/CitaService.java
+++ src/main/java/com/citas/apicitas/services/CitaService.java
@@ -1,3 +1,6 @@
+/**
+ * La interfaz CitaService define los métodos para gestionar citas en una aplicación.
+ */
 package com.citas.apicitas.services;
 
 import java.util.Set;
@@ -6,10 +9,45 @@ import com.citas.apicitas.entities.Cita;
 import com.citas.apicitas.entities.CitaId;
 
 public interface CitaService {
+
+  /**
+   * Recupera todas las citas disponibles en el sistema.
+   *
+   * @return Un conjunto de objetos Cita representando todas las citas disponibles.
+   */
   Set<Cita> findAll();
+
+  /**
+   * Busca una cita por su identificador único.
+   *
+   * @param id El identificador único de la cita a buscar.
+   * @return La cita correspondiente al identificador proporcionado, o null si no se encuentra.
+   */
   Cita findById(CitaId id);
+
+  /**
+   * Agrega una nueva cita al sistema.
+   *
+   * @param id   El identificador único de la nueva cita.
+   * @param cita La cita a agregar al sistema.
+   * @return La cita recién agregada.
+   */
   Cita addCita(CitaId id, Cita cita);
+
+  /**
+   * Modifica una cita existente en el sistema.
+   *
+   * @param id      El identificador único de la cita a modificar.
+   * @param newCita El nuevo identificador único de la cita.
+   * @return La cita modificada.
+   */
   Cita modifyCita(CitaId id, CitaId newCita);
+
+  /**
+   * Elimina una cita del sistema.
+   *
+   * @param id El identificador único de la cita a eliminar.
+   */
   void deleteCita(CitaId id);
 
 }
