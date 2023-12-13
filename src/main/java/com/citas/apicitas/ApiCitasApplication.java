4e692edf55febb534173feee51b2c5e5ae2e339a
Nicolas Ico <132182589+NicoIco97@users.noreply.github.com>
2023-12-13 15:57:58 -0500

    Documentacion

--- src/main/java/com/citas/apicitas/ApiCitasApplication.java
+++ src/main/java/com/citas/apicitas/ApiCitasApplication.java
@@ -1,13 +1,18 @@
-package com.citas.apicitas;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * La clase principal de la aplicación que inicia la ejecución de la API de Citas.
+ * Utiliza la anotación {@link org.springframework.boot.autoconfigure.SpringBootApplication}
+ * para indicar que es una clase de aplicación de Spring Boot.
+ */
 @SpringBootApplication
 public class ApiCitasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiCitasApplication.class, args);
-	}
+    /**
+     * Método principal que inicia la aplicación Spring Boot.
+     *
+     * @param args Argumentos de línea de comandos que se pueden proporcionar al iniciar la aplicación.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCitasApplication.class, args);
+    }
 
 }
