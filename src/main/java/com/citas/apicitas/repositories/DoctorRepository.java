4e1054cb5b95a13f33b606d96cdef42f812b90c8
Nicolas Ico <132182589+NicoIco97@users.noreply.github.com>
2023-12-13 16:04:41 -0500

    Docuemntacion

--- src/main/java/com/citas/apicitas/repositories/DoctorRepository.java
+++ src/main/java/com/citas/apicitas/repositories/DoctorRepository.java
@@ -1,3 +1,7 @@
+/**
+ * The {@code DoctorRepository} interface extends the Spring Data {@code CrudRepository}
+ * and provides methods for accessing and managing {@code Doctor} entities in the database.
+ */
 package com.citas.apicitas.repositories;
 
 import java.util.Set;
@@ -7,7 +11,20 @@ import org.springframework.data.repository.CrudRepository;
 import com.citas.apicitas.entities.Doctor;
 
 public interface DoctorRepository extends CrudRepository<Doctor, Long> {
+
+  /**
+   * Retrieves all doctors from the database.
+   *
+   * @return a {@code Set} containing all doctors in the database.
+   */
   Set<Doctor> findAll();
+
+  /**
+   * Retrieves all doctors from the database with a specific specialization.
+   *
+   * @param especialidad The {@code Especialidad} (specialization) of the doctors to be retrieved.
+   * @return a {@code Set} containing all doctors with the specified specialization.
+   */
   Set<Doctor> findAllByEspecialidad(Doctor.Especialidad especialidad);
 
 }
