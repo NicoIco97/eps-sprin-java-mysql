d81bd55c6e2e6f283a410067307ae4f653ba25f9
Nicolas Ico <132182589+NicoIco97@users.noreply.github.com>
2023-12-13 16:00:14 -0500

    Documentacion

--- src/main/java/com/citas/apicitas/services/PacienteService.java
+++ src/main/java/com/citas/apicitas/services/PacienteService.java
@@ -4,11 +4,47 @@ import java.util.Set;
 
 import com.citas.apicitas.entities.Paciente;
 
+/**
+ * Interface que define los servicios relacionados con la gestión de pacientes.
+ */
 public interface PacienteService {
-  Set<Paciente> findAll();
-  Paciente findById(Long id);
 
-  Paciente addPaciente(Paciente paciente);
-  Paciente modifyPaciente(Long id, Paciente paciente);
-  void deletePaciente(Long id);
+    /**
+     * Recupera todos los pacientes almacenados.
+     *
+     * @return Conjunto de pacientes.
+     */
+    Set<Paciente> findAll();
+
+    /**
+     * Busca un paciente por su identificador único.
+     *
+     * @param id Identificador único del paciente.
+     * @return Paciente encontrado, o null si no se encuentra.
+     */
+    Paciente findById(Long id);
+
+    /**
+     * Agrega un nuevo paciente al sistema.
+     *
+     * @param paciente Paciente a agregar.
+     * @return Paciente recién creado.
+     */
+    Paciente addPaciente(Paciente paciente);
+
+    /**
+     * Modifica la información de un paciente existente.
+     *
+     * @param id       Identificador único del paciente a modificar.
+     * @param paciente Nuevos datos del paciente.
+     * @return Paciente modificado.
+     */
+    Paciente modifyPaciente(Long id, Paciente paciente);
+
+    /**
+     * Elimina un paciente del sistema por su identificador único.
+     *
+     * @param id Identificador único del paciente a eliminar.
+     */
+    void deletePaciente(Long id);
 }
