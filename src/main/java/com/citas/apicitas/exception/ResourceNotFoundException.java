4cd7d0b989f496cb070bed2dc17fa0f697beb891
Nicolas Ico <132182589+NicoIco97@users.noreply.github.com>
2023-12-13 16:06:22 -0500

    Docuemntacion

--- src/main/java/com/citas/apicitas/exception/ResourceNotFoundException.java
+++ src/main/java/com/citas/apicitas/exception/ResourceNotFoundException.java
@@ -1,7 +1,17 @@
+/**
+ * Excepción personalizada para indicar que un recurso no fue encontrado.
+ * Esta excepción se lanza cuando se intenta acceder a un recurso que no existe.
+ */
 package com.citas.apicitas.exception;
 
-public class ResourceNotFoundException extends RuntimeException{
-  public ResourceNotFoundException(String msg){
-    super(msg);
-  }
+public class ResourceNotFoundException extends RuntimeException {
+
+    /**
+     * Constructor que recibe un mensaje para describir la excepción.
+     *
+     * @param msg Mensaje descriptivo de la excepción.
+     */
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
 }
