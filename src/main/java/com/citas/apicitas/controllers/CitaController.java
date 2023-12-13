9dc39d2629a19f7d717067ca8350661becb72a23
Nicolas Ico <132182589+NicoIco97@users.noreply.github.com>
2023-12-13 16:15:00 -0500

    Docuemntacion

--- src/main/java/com/citas/apicitas/controllers/CitaController.java
+++ src/main/java/com/citas/apicitas/controllers/CitaController.java
@@ -6,14 +6,7 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.citas.apicitas.entities.Cita;
 import com.citas.apicitas.entities.CitaId;
@@ -23,6 +16,9 @@ import com.citas.apicitas.services.CitaService;
 import com.citas.apicitas.services.DoctorService;
 import com.citas.apicitas.services.PacienteService;
 
+/**
+ * Controlador para gestionar las operaciones relacionadas con las citas médicas.
+ */
 @RestController
 @RequestMapping("/api/citas")
 public class CitaController {
@@ -36,17 +32,29 @@ public class CitaController {
   @Autowired
   private PacienteService pacienteService;
 
-    @GetMapping("")
-  public ResponseEntity <Set<Cita>> getCitas(){
+  /**
+   * Obtiene todas las citas médicas.
+   *
+   * @return ResponseEntity con el conjunto de citas médicas y estado HTTP 200 (OK).
+   */
+  @GetMapping("")
+  public ResponseEntity<Set<Cita>> getCitas() {
     Set<Cita> citas = citaService.findAll();
 
     return new ResponseEntity<>(citas, HttpStatus.OK);
   }
 
-  //Uso de parametros query en este endpoint
+  /**
+   * Obtiene una cita médica específica mediante parámetros de consulta.
+   *
+   * @param idPaciente ID del paciente.
+   * @param idDoctor   ID del doctor.
+   * @param fecha_hora Fecha y hora de la cita.
+   * @return ResponseEntity con la cita médica solicitada y estado HTTP 200 (OK).
+   */
   @GetMapping("/one-cita")
-  public ResponseEntity <Cita> getCita(@RequestParam Long idPaciente,
-  @RequestParam Long idDoctor, @RequestParam LocalDateTime fecha_hora){
+  public ResponseEntity<Cita> getCita(@RequestParam Long idPaciente,
+                                      @RequestParam Long idDoctor, @RequestParam LocalDateTime fecha_hora) {
 
     CitaId citaId = new CitaId(idDoctor, idPaciente, fecha_hora);
 
@@ -55,8 +63,14 @@ public class CitaController {
     return new ResponseEntity<>(cita, HttpStatus.OK);
   }
 
+  /**
+   * Agrega una nueva cita médica.
+   *
+   * @param citaId Identificador de la cita médica.
+   * @return ResponseEntity con el ID de la cita médica agregada y estado HTTP 201 (CREATED).
+   */
   @PostMapping("")
-  public ResponseEntity <CitaId> addCita(@RequestBody CitaId citaId){
+  public ResponseEntity<CitaId> addCita(@RequestBody CitaId citaId) {
     Doctor doctor = doctorService.findById(citaId.getIdProfesional());
     Paciente paciente = pacienteService.findById(citaId.getIdNumeroCedula());
 
@@ -71,9 +85,18 @@ public class CitaController {
     return new ResponseEntity<>(addedCita.getId(), HttpStatus.CREATED);
   }
 
+  /**
+   * Modifica una cita médica existente.
+   *
+   * @param citaId    Identificador de la nueva cita médica.
+   * @param idPaciente ID del paciente de la cita médica existente.
+   * @param idDoctor   ID del doctor de la cita médica existente.
+   * @param fecha_hora Fecha y hora de la cita médica existente.
+   * @return ResponseEntity con la cita médica modificada y estado HTTP 200 (OK).
+   */
   @PutMapping("")
-  public ResponseEntity <Cita> modifyCita(@RequestBody CitaId citaId, @RequestParam Long idPaciente,
-  @RequestParam Long idDoctor, @RequestParam LocalDateTime fecha_hora){
+  public ResponseEntity<Cita> modifyCita(@RequestBody CitaId citaId, @RequestParam Long idPaciente,
+                                         @RequestParam Long idDoctor, @RequestParam LocalDateTime fecha_hora) {
 
     CitaId oldCita = new CitaId(idDoctor, idPaciente, fecha_hora);
 
@@ -82,8 +105,14 @@ public class CitaController {
     return new ResponseEntity<>(modifyCita, HttpStatus.OK);
   }
 
+  /**
+   * Elimina una cita médica.
+   *
+   * @param id Identificador de la cita médica a eliminar.
+   * @return ResponseEntity con estado HTTP 204 (NO CONTENT) indicando que la cita se eliminó con éxito.
+   */
   @DeleteMapping("")
-  public ResponseEntity <Void> deleteCita(@RequestBody CitaId id){
+  public ResponseEntity<Void> deleteCita(@RequestBody CitaId id) {
     citaService.deleteCita(id);
 
     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
