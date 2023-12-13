c128e5e38758112e976287bc429682f943137443
Nicolas Ico <132182589+NicoIco97@users.noreply.github.com>
2023-12-13 16:11:51 -0500

    Documentacion

--- src/main/java/com/citas/apicitas/entities/Cita.java
+++ src/main/java/com/citas/apicitas/entities/Cita.java
@@ -1,21 +1,37 @@
-package com.citas.apicitas.entities;
-
-import jakarta.persistence.*;
-import lombok.*;
-
+/**
+ * La clase {@code Cita} representa la entidad de citas médicas en el sistema.
+ * Cada cita está asociada a un médico (Doctor) y un paciente (Paciente),
+ * identificada por la combinación de sus respectivas IDs (CitaId).
+ *
+ * @author TuNombre
+ * @version 1.0
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity(name = "cita")
 public class Cita {
+  
+  /**
+   * La clave primaria compuesta {@code id} representa la identificación única de la cita,
+   * compuesta por la combinación de las IDs del médico y del paciente.
+   */
   @EmbeddedId
   private CitaId id;
 
+  /**
+   * El médico asociado a esta cita.
+   * La relación está mapeada mediante la propiedad "idProfesional" de la clave primaria compuesta.
+   */
   @MapsId("idProfesional")
   @ManyToOne
   @JoinColumn(name = "id_profesional")
   private Doctor doctor;
 
+  /**
+   * El paciente asociado a esta cita.
+   * La relación está mapeada mediante la propiedad "idNumeroCedula" de la clave primaria compuesta.
+   */
   @MapsId("idNumeroCedula")
   @ManyToOne
   @JoinColumn(name = "id_numero_cedula")
