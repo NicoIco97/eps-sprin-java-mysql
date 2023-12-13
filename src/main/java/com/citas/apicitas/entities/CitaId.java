1bf1c0fbd3402df770074a64437d0a947befa8e0
Nicolas Ico <132182589+NicoIco97@users.noreply.github.com>
2023-12-13 16:11:03 -0500

    Docuemntacion

--- src/main/java/com/citas/apicitas/entities/CitaId.java
+++ src/main/java/com/citas/apicitas/entities/CitaId.java
@@ -1,3 +1,8 @@
+/**
+ * Clase que representa la clave compuesta (ID) para la entidad Cita.
+ * Esta clave incorpora los identificadores del profesional, el número de cédula
+ * y la fecha y hora de la cita.
+ */
 package com.citas.apicitas.entities;
 
 import java.io.Serializable;
@@ -11,7 +16,19 @@ import lombok.*;
 @NoArgsConstructor
 @Embeddable
 public class CitaId implements Serializable {
-  private long idProfesional;
-  private long idNumeroCedula;
-  private LocalDateTime fechaHora;
+
+    /**
+     * Identificador del profesional asociado a la cita.
+     */
+    private long idProfesional;
+
+    /**
+     * Número de cédula asociado a la cita.
+     */
+    private long idNumeroCedula;
+
+    /**
+     * Fecha y hora de la cita.
+     */
+    private LocalDateTime fechaHora;
 }
